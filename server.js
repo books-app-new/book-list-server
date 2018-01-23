db1b097a0147c28d0634eefc9a41ceafa20ecae9
Jay Adams <j.adams011@hotmail.com>
2018-01-23 09:14:21 -0800

    fixing DB pointers

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   response.sendFile('./public/index.html');
 // });
 
-app.get('/db/person', function(request, response) {
-  client.query('SELECT * FROM persons;')
+app.get('DATABASE_URL', function(request, response) {
+  client.query('SELECT * FROM DATABASE_URL;')
   .then(function(data) {
     response.send(data);
   })
@@ -30,26 +30,47 @@ app.get('/db/person', function(request, response) {
   });
 });
 
-app.post('/db/person', function(request, response) {
-  client.query(`
-    INSERT INTO persons(name, age, ninja)
-    VALUES($1, $2, $3);
-    `,
-    [
-      request.body.name,
-      request.body.age,
-      request.body.ninja
-    ]
-  )
-  .then(function(data) {
-    response.redirect('/');
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-});
+loadDB();
 
-createTable();
+function loadBooks() {
+ fs.readFile('../book-list-client/data/books.json', function(err, fd) {
+   JSON.parse(fd.toString()).forEach(function(ele) {
+     client.query(
+       'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
+       [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
+     )
+   })
+ })
+}
+
+function loadDB() {
+ client.query(`
+   CREATE TABLE IF NOT EXISTS
+   books(id SERIAL PRIMARY KEY, title VARCHAR(255), author VARCHAR(255), isbn VARCHAR(255), image_url VARCHAR(255), description TEXT NOT NULL);
+   `)
+
+   .then(loadBooks());
+}
+// app.post('/db/person', function(request, response) {
+//   client.query(`
+//     INSERT INTO persons(name, age, ninja)
+//     VALUES($1, $2, $3);
+//     `,
+//     [
+//       request.body.name,
+//       request.body.age,
+//       request.body.ninja
+//     ]
+//   )
+//   .then(function(data) {
+//     response.redirect('/');
+//   })
+//   .catch(function(err) {
+//     console.error(err);
+//   });
+// });
+
+// createTable();
 
 app.listen(PORT, () => {
   console.log(`currently listening on ${PORT}`);
